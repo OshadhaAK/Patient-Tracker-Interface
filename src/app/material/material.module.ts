8b652212c39d76bb6766345b052030fd059e3532
oshadhaAK <oshadhakatulanda.16@cse.mrt.ac.lk>
2020-04-01 16:29:57 -0400

    dashboard update

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule } from '@angular/material';
 import { from } from 'rxjs';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 const MaterialComponents = [
   MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
-  MatIconModule
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule
 ];
 
 @NgModule({
